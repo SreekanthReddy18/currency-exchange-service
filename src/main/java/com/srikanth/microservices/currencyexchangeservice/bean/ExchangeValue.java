61f0fdd187ed0d494a731d133ba26b33520d1948
SreekanthReddy18 <sreekanth3143@gmail.com>
2019-04-14 00:09:19 +0530

    dynamic port setting done.

--- src/main/java/com/srikanth/microservices/currencyexchangeservice/bean/ExchangeValue.java
+++ src/main/java/com/srikanth/microservices/currencyexchangeservice/bean/ExchangeValue.java
@@ -15,6 +15,14 @@ public class ExchangeValue {
 	private String from;
 	private String to;
 	private BigDecimal conversionMultiple;
+	private int port;
+	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
 	
 	
 	
